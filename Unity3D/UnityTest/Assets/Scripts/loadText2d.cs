e47f57fb3e4471a3094c8c8cbe1b8df64e744765
rise <rise.worlds@outlook.com>
2015-01-15 00:45:05 +0800

    学习Unity3d

---
+++ Unity3D/UnityTest/Assets/Scripts/loadText2d.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadText2d : MonoBehaviour {
+	private Texture2D txt;
+	Texture2D[] array;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		// 加载贴图
+		if (GUI.Button(new Rect(10, 50, 90, 90), "加载一张图片"))
+		{
+			txt = Resources.Load<Texture2D>("Icons/avatar_icon1");
+		}
+		if (GUI.Button(new Rect(10, 150, 90,90), "加载所有图片"))
+		{
+			array = Resources.LoadAll<Texture2D>("Icons");
+		}
+		// 绘制贴图
+		if (null != txt)
+		{
+			GUI.DrawTexture(new Rect(150, 50, 90, 90), txt);
+		}
+		if (null != array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				GUI.DrawTexture(new Rect(150 + i * 95, 150, 90, 90), array[i], ScaleMode.ScaleToFit, true, 0);
+			}
+		}
+	}
+}
---
+++ Unity3D/UnityTest/Assets/Scripts/modelMove.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class modelMove : MonoBehaviour
+	{
+		public float speed = 2;
+		void Start()
+		{
+
+		}
+		void Update()
+		{
+			Vector3 oldPostion = transform.position;
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.Translate(Vector3.forward * Time.deltaTime * speed, Space.World);
+				animation.Play("run");
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.Translate(Vector3.back * Time.deltaTime * speed, Space.World);
+				animation.Play("run");
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+				animation.Play("run");
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				transform.Translate(Vector3.right * Time.deltaTime * speed, Space.World);
+				animation.Play("run");
+			}
+			transform.LookAt(transform.position + transform.position - oldPostion);
+		}
+	}
+}
