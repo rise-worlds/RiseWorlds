c926509dcbaea33ff52167bea17aa0c8a2141f38
Rise Worlds <Rise Worlds>
2016-07-17 21:22:24 +0800

    test
    Signed-off-by: Rise Worlds <Rise Worlds>

---
+++ Unity3D/HelloWorld/Assets/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.LogWarning ("Update");
+	}
+
+	void OnGUI()
+	{
+		GUI.skin.label.fontSize = 100;
+		GUI.Label (new Rect (10, 10, Screen.width, Screen.height), "Hello World!");
+	}
+}
